82999c0fe98f4553bf048c1d4b0b4a5c167fd3ff
Sarah Ortega <snortega1999@gmail.com>
2023-07-25 13:42:28 -0400

    added the connection manager

---
+++ src/com/team/progress_tracker/ConnectionManager.java
@@ -0,0 +1,44 @@
+package com.team.progress_tracker;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionManager {
+	
+	private static final String URL = "jdbc:mysql://localhost:3306/BookDatabase";
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
+	
+	private static Connection connection = null;
+	
+	private static void createConnection() {
+		
+		try {
+			Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+			connection = conn;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() {
+		if (connection == null) {
+			createConnection();
+		}
+		return connection;	
+	}
+
+	public static void main(String[] args) {
+		Connection conn = getConnection();
+		System.out.println("Connecton Established!");
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
