0736d0d446f6c534847e89de76936a0dbddaa5eb
Dominic Swaby <domswaby@gmail.com>
2023-07-25 14:28:18 -0400

    give connection string correct snake_case name

--- src/com/team/progress_tracker/ConnectionManager.java
+++ src/com/team/progress_tracker/ConnectionManager.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionManager {
 	
-	private static final String URL = "jdbc:mysql://localhost:3306/BookDatabase";
+	private static final String URL = "jdbc:mysql://localhost:3306/book_database";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "root";
 	
