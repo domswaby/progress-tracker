9c5f0f1e409d9e2dcc3e8e3b2c94f6f0ead6ad17
Sarah Ortega <snortega1999@gmail.com>
2023-07-26 13:08:50 -0400

    fixed main and menu to align with MVP standards

--- src/com/team/progress_tracker/Main.java
+++ src/com/team/progress_tracker/Main.java
@@ -17,11 +17,11 @@ public class Main {
 		username = Menu.startupResult(result); // handles the creation of a new account or the login of an old one
 		
 		while (true) {
-			if (result != 5) { //if the user doesn't want to log out
+			if (result != 3) { //if the user doesn't want to log out; edit to 4 when we add more
 				result = Menu.userMenu(username);
 				Menu.userMenuResult(result);
 			} else {
-				result = Menu.startupMenu(reader); //if the user did log out, the system promps them to log back in or shut down
+				result = Menu.startupMenu(reader); //if the user did log out, the system prompts them to log back in or shut down
 				username = Menu.startupResult(result);
 			}
 		}
